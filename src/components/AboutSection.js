70db4783c5c2cf1e348c4036020f6459fe7dd3f4
Daniel Binoy <dbinoy15@gmail.com>
2020-09-20 11:57:55 -0700

    set up container

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,6 +6,22 @@ import Container from 'react-bootstrap/Container'
 import SectionHeader from './SectionHeader'
 import Fade from 'react-reveal/Fade'
 import NavLink from './NavLink'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+function CodeWindow({ info }) {
+    return (
+        <div className="code-window-wrapper">
+
+        </div>
+    );
+}
+
+
+
+
+
+
 
 function AboutSection() {
     const description = 'Hi! I\'m Daniel! I\'m a computer science student at the Donald Bren ' +
@@ -17,20 +33,40 @@ function AboutSection() {
 
     return (
         <section id="about">
-            <Container className="h-100 text-center">
-            
-                <div className = "about-stuff">
-                    <Fade top>
-                        <Image className="masthead-profile" src={ProfileImg} roundedCircle />
-                        <SectionHeader text="About me" />
-
-                    </Fade>
-                    <Fade bottom>
+            <Container className="h-100 w-100 text-center">
+                <div className="about-stuff">
+                    <Row >
+                        <Col>
+                        <Fade top>
+                            <Image className="masthead-profile" src={ProfileImg} roundedCircle />
+                        </Fade>
+                        </Col>
+                    </Row>
+                    <Row >
+                        <Col>
+                        <Fade top>
+                            <SectionHeader text="About me" />
+                        </Fade>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
                         <p className="about-me-paragraph">{description}</p>
-                        <div className="skills-btn">
-                            <NavLink className="btn btn-outline-light skills-btn" destination="skills" text="Skills"></NavLink>
-                        </div>
-                    </Fade>
+                        </Col>
+                        <Col>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                        <Fade bottom>
+                            
+                            <div className="skills-btn">
+                                <NavLink className="btn btn-outline-light skills-btn" destination="skills" text="Skills"></NavLink>
+                            </div>
+                        </Fade>
+                        </Col>
+                    </Row>
                 </div>
 
             </Container>
