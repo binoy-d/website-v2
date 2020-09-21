5844137dbf41636e99997a8f6a9ce8c0ba952071
Daniel Binoy <dbinoy15@gmail.com>
2020-09-21 11:04:28 -0700

    Updates

---
+++ precache-manifest.66d45eb831ef268419170e08808a8448.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9c3ca1aa852285ea2957b60dc14834",
+    "url": "/binoy.co/index.html"
+  },
+  {
+    "revision": "6709bb73069e743fb51d",
+    "url": "/binoy.co/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9591cfe417def83b2dcb",
+    "url": "/binoy.co/static/css/main.7e9fe58a.chunk.css"
+  },
+  {
+    "revision": "6709bb73069e743fb51d",
+    "url": "/binoy.co/static/js/2.05506ae2.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/binoy.co/static/js/2.05506ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9591cfe417def83b2dcb",
+    "url": "/binoy.co/static/js/main.1238b9c8.chunk.js"
+  },
+  {
+    "revision": "51981b02e3cf9e62a501",
+    "url": "/binoy.co/static/js/runtime-main.9a147134.js"
+  },
+  {
+    "revision": "36b4ba6db6ec415fd91e435d8a824ea6",
+    "url": "/binoy.co/static/media/bubbles-together.36b4ba6d.png"
+  },
+  {
+    "revision": "20e3ec16fb55fe4b60aa4c08ae31bf5c",
+    "url": "/binoy.co/static/media/bubblify.20e3ec16.png"
+  },
+  {
+    "revision": "00dcd0a9d835cccea1200f16651e3f66",
+    "url": "/binoy.co/static/media/color-game.00dcd0a9.png"
+  },
+  {
+    "revision": "167189ab8c372f329e3b618e7a20f351",
+    "url": "/binoy.co/static/media/daniel-profile.167189ab.jpg"
+  },
+  {
+    "revision": "0db173e48cea39a077c61c0cfbc92ec0",
+    "url": "/binoy.co/static/media/debug-duck.0db173e4.png"
+  },
+  {
+    "revision": "3648b303373f846b7e8c4f4c456ebf7f",
+    "url": "/binoy.co/static/media/pong.3648b303.png"
+  },
+  {
+    "revision": "16dd7b3ac4d6c0e0e3cb2e918127e435",
+    "url": "/binoy.co/static/media/puzzle-game.16dd7b3a.PNG"
+  },
+  {
+    "revision": "a0bd69978b60db67f00b88f42364193d",
+    "url": "/binoy.co/static/media/todo-list.a0bd6997.png"
+  },
+  {
+    "revision": "b3c0b21417077c5ee6c21b45929f655a",
+    "url": "/binoy.co/static/media/website.b3c0b214.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3067c8f6f0154303f784913dd36b3f36.js"
+  "/binoy.co/precache-manifest.66d45eb831ef268419170e08808a8448.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/binoy.co/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
