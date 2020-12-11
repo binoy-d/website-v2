bc70e27eb9476428352590c7d82d1bfed5577f3e
Daniel Binoy <dbinoy15@gmail.com>
2020-12-10 19:35:16 -0800

    overlay skills header

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -14,36 +14,47 @@ import {skills} from './data'
 class SkillsIconBackground extends React.Component {
     constructor(props) {
         super(props);
-    
         this.state = { x: 0, y: 0 };
       }
     
       _onMouseMove(e) {
         this.setState({ x: e.screenX, y: e.screenY });
-        console.log(this.state.x);
-        document.getElementById("skills-pattern-wrapper").style.setProperty("--x", -this.state.x+"px");
-        document.getElementById("skills-pattern-wrapper").style.setProperty("--y", -this.state.y+"px")
+        document.getElementById("skills-background").style.setProperty("--x", -this.state.x+"px");
+        document.getElementById("skills-background").style.setProperty("--y", -this.state.y+"px")
       }
 
     render() {
         const { x, y } = this.state;
       return (
-        <div onMouseMove={this._onMouseMove.bind(this)} id = "skills-pattern-wrapper">
+        <div id = "skills-wrapper" onMouseMove={this._onMouseMove.bind(this)} >
+            <div className = "skills-content">
+                {this.props.children}
+            </div>
+            <div id = "skills-background">
 
+            </div>
+            
         </div>
       );
     }
   }
 
 
+
+
+
 function SkillsSection() {
     return (
         <section id="skills">
-            <SkillsIconBackground />
+            
+            <SkillsIconBackground>
+                <SectionHeader className = "over" text="Skills" />
+            </SkillsIconBackground>
+            
             <Container className="">    
                 <div className="skills-stuff">
                     <Fade top>
-                        <SectionHeader text="Skills" />
+                        
                     </Fade>                
                 </div>
             </Container>
